93fa8040ddcce6ac088424fb479cd1287cfaeaf5
Alexander Loboda <loboda@rain.ifmo.ru>
2018-03-30 17:13:17 +0300

    cli

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,156 @@
 import ctlab.bn.BayesianNetwork;
 import ctlab.bn.Variable;
 import ctlab.mcmc.Model;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.Arrays.asList;
 
 public class Main {
-    private static List<Variable> parse_ge_table(String filename) throws FileNotFoundException {
+    private static File ge_file;
+    private static File output;
+    private static File log;
+
+    private static int n_steps;
+    private static int warmup_steps;
+    private static int executors;
+    private static int disc_limit;
+    private static int n_cores;
+
+    private static boolean parse_args(String[] args) {
+        OptionParser optionParser = new OptionParser();
+        optionParser.allowsUnrecognizedOptions();
+        optionParser.acceptsAll(asList("h", "help"), "Print a short help message");
+        OptionSet optionSet = optionParser.parse(args);
+
+        OptionSpec<String> ge = optionParser.acceptsAll(asList("g", "ge"),
+                "gene expression data file").withRequiredArg().ofType(String.class).required();
+        OptionSpec<Integer> steps = optionParser.acceptsAll(asList("s", "steps"),
+                "Number of steps for each run").withRequiredArg().ofType(Integer.class).required();
+        OptionSpec<Integer> warmup = optionParser.acceptsAll(asList("w", "warmup"),
+                "Number of steps before main loop").withRequiredArg().ofType(Integer.class).defaultsTo(0);
+        OptionSpec<Integer> exec = optionParser.acceptsAll(asList("r", "runs"),
+                "Number of independent runs").withRequiredArg().ofType(Integer.class).defaultsTo(1);
+        OptionSpec<Integer> bound = optionParser.acceptsAll(asList("b", "disc_limit"),
+                "Limit on number of discretization algorithm steps").withRequiredArg().ofType(Integer.class)
+                .defaultsTo(5);
+        OptionSpec<String> outfile = optionParser.acceptsAll(asList("o", "out"),
+                "output file").withRequiredArg().ofType(String.class);
+        OptionSpec<String> log_file = optionParser.acceptsAll(asList("l", "log"),
+                "log directory").withRequiredArg().ofType(String.class);
+        OptionSpec<Integer> cores = optionParser.acceptsAll(asList("c", "cores"),
+                "number of cores").withRequiredArg().ofType(Integer.class).defaultsTo(1);
+
+        if (optionSet.has("h")) {
+            try {
+                optionParser.printHelpOn(System.out);
+            } catch (IOException ignored) { }
+            return false;
+        }
+
+        optionSet = optionParser.parse(args);
+
+        n_steps = optionSet.valueOf(steps);
+        warmup_steps = optionSet.valueOf(warmup);
+        executors = optionSet.valueOf(exec);
+        disc_limit = optionSet.valueOf(bound);
+
+        ge_file = new File(optionSet.valueOf(ge));
+        output = new File(optionSet.valueOf(outfile));
+        log = new File(optionSet.valueOf(log_file));
+
+        return true;
     }
 
-    public static void main(String[] args) throws IOException {
-        String gene_file = args[0];
+    private static List<Variable> parse_ge_table(File file) throws FileNotFoundException {
+        List<Variable> res = new ArrayList<>();
+        List<String> names = new ArrayList<>();
+        List<List<Double>> data = new ArrayList<>();
+        try (Scanner sc = new Scanner(file)) {
+            String firstLine = sc.nextLine();
+            Scanner line_sc = new Scanner(firstLine);
+            while(line_sc.hasNext()) {
+                names.add(line_sc.next());
+            }
 
-        int steps = Integer.parseInt(args[1]);
-        int warmup = Integer.parseInt(args[2]);
-        int models_num = Integer.parseInt(args[3]);
-        int disc_bound = Integer.parseInt(args[4]);
+            int n = names.size();
+            for (int i = 0; i < n; i++) {
+                data.add(new ArrayList<>());
+            }
 
-        String out_file = args[5];
-        String log_dir = args[6];
+            while (sc.hasNext()) {
+                for (int i = 0; i < n; i++) {
+                    data.get(i).add(sc.nextDouble());
+                }
+            }
 
-        List<Variable> genes = parse_ge_table(gene_file);
+            for (int i = 0; i < n; i++) {
+                res.add(new Variable(names.get(i), data.get(i), disc_limit));
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        if (!parse_args(args)) {
+            System.exit(0);
+        }
+
+        List<Variable> genes = parse_ge_table(ge_file);
         int n = genes.size();
         BayesianNetwork bn = new BayesianNetwork(genes, false, true);
 
         List<Model> models = new ArrayList<>();
 
-        List<Edge> edges = count_hits(n, models);
-
-        for (int i = 0; i < models_num; i++) {
-
+        for (int i = 0; i < executors; i++) {
+            models.add(new Model(new BayesianNetwork(bn), disc_limit));
         }
 
-        edges.forEach(x -> x.scale((steps * models_num)));
-        try(PrintWriter pw = new PrintWriter(out_file)) {
+        ExecutorService es = Executors.newFixedThreadPool(n_cores);
+        models.forEach(x -> es.submit(new Task(x, n_steps, warmup_steps)));
+        es.shutdown();
+        es.awaitTermination(1_000_000, TimeUnit.HOURS);
+
+        List<Edge> edges = count_hits(n, models);
+
+        edges.forEach(x -> x.scale((n_steps * executors)));
+        try(PrintWriter pw = new PrintWriter(output)) {
             for (Edge e: edges) {
                 pw.println(genes.get(e.v).getName() + "\t" + genes.get(e.u).getName() + "\t" + e.weight);
             }
+        }
+    }
+
+    private static class Task implements Runnable {
+        private int steps;
+        private Model m;
+        private int warmup;
+
+        Task(Model m, int steps, int warmup) {
+            this.m = m;
+            this.steps = steps;
+            this.warmup = warmup;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < warmup; i++) {
+                m.step(true);
+            }
+            for (int i = 0; i < steps; i++){
+                m.step(false);
+            }
+            m.finish();
         }
     }
 
