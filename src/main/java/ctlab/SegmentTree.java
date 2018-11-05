5efc35c2d8c00482ba3aee556df927afc8dcdc06
Alexander Loboda <aleks.loboda@gmail.com>
2018-11-05 18:10:00 -0500

    removed commited code

--- src/main/java/ctlab/SegmentTree.java
+++ src/main/java/ctlab/SegmentTree.java
@@ -91,44 +91,4 @@ public class SegmentTree {
             current /= sum;
         }
     }
-
-    //public Action cutoff() {
-    //    Collections.sort(actions);
-    //    Collections.reverse(actions);
-    //    int u = actions.get(0).u();
-    //    List<Action> remain = actions.subList(maxSize - 1, actions.size());
-    //    Action result = null;
-    //    Action toAdd = null;
-
-    //    double[] lls = remain.stream().mapToDouble(Action::loglik).toArray();
-
-    //    if (lls.length != 0) {
-    //        double maxLL = Arrays.stream(lls).max().getAsDouble();
-    //        double threshold = logEPS - Math.log(lls.length);
-    //        lls = Arrays.stream(lls)
-    //                .map(x -> x - maxLL)
-    //                .filter(x -> x > threshold)
-    //                .map(Math::exp)
-    //                .toArray();
-    //        double sum = Arrays.stream(lls).sum();
-    //        double geneLL = maxLL - Math.log(sum);
-    //        toAdd = new Action(ActionType.GENE, u, u, geneLL);
-    //        double[] ps = Arrays.stream(lls).map(x -> x / sum).toArray();
-    //        int i = 0;
-    //        double rvalue = ThreadLocalRandom.current().nextDouble();
-    //        double prefix = 0.0;
-    //        for (double p: ps) {
-    //            prefix += p;
-    //            if (rvalue < prefix + EPS) {
-    //                result = remain.get(i);
-    //                break;
-    //            }
-    //            ++i;
-    //        }
-    //    }
-
-    //    remain.clear();
-    //    if (toAdd != null) {
-    //        actions.add(toAdd);
-    //    }
 }
