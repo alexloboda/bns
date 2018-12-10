5e9b34b4bbff668a9b4fb7c5975709908516e6f3
Alexander Loboda <aleks.loboda@gmail.com>
2018-12-10 17:45:09 -0500

    No debug output

--- src/test/java/ctlab/bn/action/MultinomialTest.java
+++ src/test/java/ctlab/bn/action/MultinomialTest.java
@@ -8,7 +8,7 @@ import java.util.SplittableRandom;
 import java.util.function.Function;
 
 public class MultinomialTest {
-    private final static int NCHOICES = 200000;
+    private final static int NCHOICES = 20000;
 
     @Test
     public void multinomialTest() {
@@ -34,9 +34,6 @@ public class MultinomialTest {
 
         int sum = Arrays.stream(hits).sum();
         double[] fs = Arrays.stream(hits).mapToDouble(x -> (double)x / sum).toArray();
-        for (double d: fs) {
-            System.out.println(d);
-        }
         for (int i = 0; i < ps.length; i++) {
             Assert.assertEquals(ps[i] / psSum, fs[i], 0.01);
         }
