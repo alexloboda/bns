37ed23d10f5ce260cf09315571679ed69f961590
maxLundin <maxLundin3@gmail.com>
2021-06-19 18:22:07 +0300

    RGBM update

--- rgbm.R
+++ rgbm.R
@@ -7,7 +7,6 @@ df <- data.table::fread(filename, data.table = F)
 tmp = dim(df)
 x = tmp[1]
 y = tmp[2]
-
 m <- as.matrix(df)
 K <- matrix(0, x, y)
 colnames(K) <- colnames(m)
@@ -17,16 +16,6 @@ cl <- makeCluster(32)
 res <- RGBM(E = m, K, g_M = matrix(1, 195, y), tfs = paste0("G", c(1:195)),
             targets = paste0("G", 1:y))
 stopCluster(cl)
-
-res_df <- data.frame(from = c(), to = c(), score = c())
-for (i in 1:nrow(res)) {
-  for (j in 1:ncol(res)) {
-    if (i != j) {
-       res_df <- rbind(res_df, list(from = rownames(res)[i], to = colnames(res)[j],
-                       score = res[i, j]))
-    }
-  }
-}
-res_df <- res_df[order(res_df[, 3], decreasing = TRUE), ]
-data.table::fwrite(res_df, args[2], col.names = FALSE, sep = "\t")
 warnings()
+write.table(res,file="test.txt")
+system(paste("python3 RGBM.py >", args[2], sep=" "))
